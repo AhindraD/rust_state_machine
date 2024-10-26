97aef47d2a90081a09463da83170f9b58a8823ca
Ahindra D. <official.ahindra@gmail.com>
2024-10-26 11:31:37 +0530

    new_balances_instance

--- src/balances.rs
+++ src/balances.rs
@@ -1,1 +1,14 @@
-pub struct Pallet{}
+use std::collections::BTreeMap;
+pub struct Pallet {
+    //a key-val pair storing balance
+    balances: BTreeMap<String, u128>,
+}
+
+impl Pallet {
+    //Creating a new instance of the balances module
+    pub fn new() -> Self {
+        Self {
+            balances: BTreeMap::new(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use balances::Pallet;
+
 mod balances;
 fn main() {
     println!("Hello from Rust State Machine!");
+    let mut pallet: Pallet = balances::Pallet::new();
 }
