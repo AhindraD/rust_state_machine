f6d08bd1ed4f1c6f5d67bbae52a5f467c728d69b
Ahindra D. <official.ahindra@gmail.com>
2024-11-05 12:14:41 +0530

    options_patternMatch

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
 use balances::Pallet;
-
+use std::collections::BTreeMap;
 mod balances;
 fn main() {
     println!("Hello from Rust State Machine!");
     let mut pallet: Pallet = balances::Pallet::new();
+
+    let mut map: BTreeMap<&str, i32> = BTreeMap::new();
+    map.insert("ahindra", 777);
+    assert_eq!(map.get(&"ahindra"), Some(&777));
+    assert_eq!(map.get(&"anon"), None);
+
+    let maybe_val1 = map.get("ahindra");
+    let maybe_val2 = map.get("zyzz");
+    match maybe_val1 {
+        Some(value) => {
+            println!("{}", value)
+        }
+        None => {
+            println!("not present")
+        }
+    }
+    match maybe_val2 {
+        Some(value) => {
+            println!("{}", value)
+        }
+        None => {
+            println!("not present")
+        }
+    }
 }
